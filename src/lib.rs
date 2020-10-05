a2fd6cbf84873370623f1378de6b17f8e55b2f5a
Vivien <vivien.boucher@outlook.com>
2020-10-05 16:02:58 +0200

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-use bitcoin::hash_types::BlockHash;
-use bitcoin::{Block, BlockHeader};
+// use bitcoin::hash_types::BlockHash;
+// use bitcoin::{Block, BlockHeader};
 extern crate reqwest;
 extern crate serde;
 use serde::Deserialize;
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 pub struct Client {
     pub url: String,
 }
+
 #[derive(Deserialize, Debug)]
 pub struct BlockFormat {
     pub id: String,
@@ -23,6 +24,12 @@ pub struct BlockFormat {
     pub weight: u32,
     pub previousblockhash: String,
 }
+#[derive(Deserialize, Debug)]
+pub struct BlockStatus {
+    pub in_best_chain: bool,
+    pub next_best : String,
+    pub height: u32,
+}
 
 #[derive(Deserialize, Debug)]
 pub struct VoutFormat {
@@ -35,19 +42,19 @@ pub struct VoutFormat {
 
 #[derive(Deserialize, Debug)]
 pub struct VinFormat {
-    txid: String,
-    vout: u32,
-    is_coinbase: bool,
-    scriptsig: String,
-    scriptsig_asm: String,
+    pub txid: String,
+    pub vout: u32,
+    pub is_coinbase: bool,
+    pub scriptsig: String,
+    pub scriptsig_asm: String,
     //FIXME
     // inner_redeemscript_asm: String,
     // inner_witnessscript_asm: String,
-    sequence: u32,
+    pub sequence: u32,
     //FIXME
     // witness[]
     // #[serde(skip_deserializing)]
-    prevout: Option<VoutFormat>,
+    pub prevout: Option<VoutFormat>,
 }
 #[derive(Deserialize, Debug)]
 pub struct TxStatusFormat {
@@ -58,27 +65,27 @@ pub struct TxStatusFormat {
 }
 #[derive(Deserialize, Debug)]
 pub struct TransactionFormat {
-    txid: String,
-    version: u32,
-    locktime: u32,
-    size: u32,
-    weight: u32,
-    fee: u32,
-    vin: Vec<VinFormat>,
-    vout: Vec<VoutFormat>,
-    status: TxStatusFormat,
+    pub txid: String,
+    pub version: u32,
+    pub locktime: u32,
+    pub size: u32,
+    pub weight: u32,
+    pub fee: u32,
+    pub vin: Vec<VinFormat>,
+    pub vout: Vec<VoutFormat>,
+    pub status: TxStatusFormat,
 }
 
 impl Client {
-    pub fn new(url: String) -> Self {
-        Client { url }
+    pub fn new(url: &str) -> Self {
+        Client { url : url.to_string()}
     }
     // GET /block/:hash
 
     // Returns information about a block.
     // Available fields: id, height, version, timestamp, bits, nonce, merkle_root, tx_count, size, weight and previousblockhash. Elements-based chains have an additional proof field. See block format for more details.
     // The response from this endpoint can be cached indefinitely.
-    pub fn get_block(&self, hash: String) -> Result<BlockFormat, Box<dyn std::error::Error>> {
+    pub fn get_block(&self, hash: &str) -> Result<BlockFormat, Box<dyn std::error::Error>> {
         let request_url = format!("{}{}{}", self.url, "/block/", hash);
         let resp: BlockFormat = reqwest::blocking::get(&request_url)?.json()?;
         Ok(resp)
@@ -89,10 +96,10 @@ impl Client {
     // Available fields: in_best_chain (boolean, false for orphaned blocks), next_best (the hash of the next block, only available for blocks in the best chain).
     pub fn get_block_status(
         &self,
-        hash: String,
-    ) -> Result<BlockFormat, Box<dyn std::error::Error>> {
+        hash: &str,
+    ) -> Result<BlockStatus, Box<dyn std::error::Error>> {
         let request_url = format!("{}{}{}{}", self.url, "/block/", hash, "/status");
-        let resp: BlockFormat = reqwest::blocking::get(&request_url)?.json()?;
+        let resp: BlockStatus = reqwest::blocking::get(&request_url)?.json()?;
         Ok(resp)
     }
     // GET /block/:hash/txs[/:start_index]
@@ -102,8 +109,8 @@ impl Client {
     // The response from this endpoint can be cached indefinitely.
     pub fn get_block_txs(
         &self,
-        hash: String,
-        start_index: Option<u32>,
+        hash: &str,
+        start_index: Option<i32>,
     ) -> Result<Vec<TransactionFormat>, Box<dyn std::error::Error>> {
         let i = start_index.unwrap_or(0);
         let mut request_url = format!("{}{}{}{}", self.url, "/block/", hash, "/txs");
@@ -141,4 +148,36 @@ impl Client {
     // GET /blocks/tip/hash
 
     // Returns the hash of the last block.
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    static ENDPOINT_URL : &str = "https://blockstream.info/testnet/api/";
+    fn default_client()-> Client{
+       return Client::new(ENDPOINT_URL);
+    }
+    #[test]
+    fn test_get_block() {
+
+        let client = default_client();
+        let response = client.get_block("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7");
+        assert_eq!(response.unwrap().previousblockhash,"000000000000002b6f0830e1b92c6e59f18d147c0370a3425c91be21e0b1ff85");
+    }
+    #[test]
+    fn test_get_block_status(){
+        let client = default_client();
+        let response = client.get_block_status("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7");
+        assert_eq!(response.unwrap().in_best_chain,true);
+    }
+    #[test]
+    fn get_block_txs_with_and_without_index(){
+        let client= default_client();
+        let first_txs_index = client.get_block_txs("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7", None);
+        let second_txs_index = client.get_block_txs("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7", Some(25));
+        assert_eq!(first_txs_index.unwrap().iter().position(|tx| tx.txid == "bdbaa506c8903918b407fca86bd3498cd7794000b22cddeb1c87c2d9eb8fab62").unwrap(),0);
+        assert_eq!(second_txs_index.unwrap().iter().position(|tx| tx.txid == "a9e7e29b703e667311fb2453e694f17d178822cc2fc4fe4db8cfb8df81898845").unwrap(),0);
+    }
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod lib;
 // use futures::executor;
  fn main() {
-   let client = lib::Client::new("https://elc-t.zqsd.io".to_string());
-    let blockjson = client.get_block(String::from("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7"));
-    println!("{:?}",blockjson);
-    let blocktx = client.get_block_txs(String::from("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7"),None);
-    println!("{:?}",blocktx);
+   let client = lib::Client::new("https://elc-t.zqsd.io");
+    // let blockjson = client.get_block("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7");
+    // println!("{:?}",blockjson);
+    // let get_block_status = client.get_block_status("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7");
+    // println!("{:?}",get_block_status);
+    let blocktx = client.get_block_txs("000000000000003aaa3b99e31ed1cac4744b423f9e52ada4971461c81d4192f7",Some(0));
+    println!("{:?}",blocktx.unwrap().iter().position(|tx| tx.txid == "bdbaa506c8903918b407fca86bd3498cd7794000b22cddeb1c87c2d9eb8fab62").unwrap());
+
 }
